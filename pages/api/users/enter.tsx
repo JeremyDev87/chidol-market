d578ac6eca75e05d3bf62e3566728f46664c8f4d
Jeremy Winchester Park <soundbrokaz@kakao.com>
2022-03-02 13:32:38 +0900

    twilio setup

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,6 +1,9 @@
 import client from "@libs/server/client";
+import twilio from "twilio";
 import withHandler, { ResponseType } from "@libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
+
+const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
 
 async function handler(
 	req: NextApiRequest,
@@ -31,6 +34,13 @@ async function handler(
 		},
 	});
 	console.log(token);
+	if (phone) {
+		await twilioClient.messages.create({
+			messagingServiceSid: process.env.TWILIO_MSID,
+			to: process.env.MYPHONE!,
+			body: `인증번호 : ${payload}`,
+		});
+	}
 	return res.status(200).json({
 		ok: true,
 	});
