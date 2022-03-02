279dfc2d56cfaf890b6a0e70c788b807ffa25a2e
Jeremy Winchester Park <soundbrokaz@kakao.com>
2022-03-02 13:01:18 +0900

    id token maker

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,9 +1,39 @@
-import withHandler from "@libs/server/withHandler";
+import client from "@libs/server/client";
+import withHandler, { ResponseType } from "@libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
 
-async function handler(req: NextApiRequest, res: NextApiResponse) {
-	console.log(req.body);
-	return res.status(200).end();
+async function handler(
+	req: NextApiRequest,
+	res: NextApiResponse<ResponseType>
+) {
+	const { email, phone } = req.body;
+	const user = phone ? { phone: +phone } : email ? { email } : null;
+	if (!user) {
+		return res.status(400).json({
+			ok: false,
+		});
+	}
+	const payload = Math.floor(100000 + Math.random() * 900000) + "";
+	const token = await client.token.create({
+		data: {
+			payload,
+			user: {
+				connectOrCreate: {
+					where: {
+						...user,
+					},
+					create: {
+						name: "Anoymous",
+						...user,
+					},
+				},
+			},
+		},
+	});
+	console.log(token);
+	return res.status(200).json({
+		ok: true,
+	});
 }
 
 export default withHandler("POST", handler);
