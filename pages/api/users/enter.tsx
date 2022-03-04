112df3cd28f5422ecf1e4b6522a516f15910f1eb
Jeremy Winchester Park <soundbrokaz@kakao.com>
2022-03-04 13:19:06 +0900

    sendgrid mail failed replace nodemailer

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,8 +1,18 @@
 import client from "@libs/server/client";
 import twilio from "twilio";
+import mail from "@sendgrid/mail";
 import withHandler, { ResponseType } from "@libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
 
+const nodemailer = require("nodemailer");
+const transporter = nodemailer.createTransport({
+	service: "gmail",
+	auth: {
+		user: process.env.GMAIL_ID,
+		pass: process.env.GMAIL_PW,
+	},
+});
+// mail.setApiKey(process.env.SENDGRID_KEY!);
 const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
 
 async function handler(
@@ -40,6 +50,21 @@ async function handler(
 			to: process.env.MYPHONE!,
 			body: `인증번호 : ${payload}`,
 		});
+	} else if (email) {
+		const sendEmail = await transporter
+			.sendMail({
+				from: `soundbrokaz <soundbrokaz@gmail.com>`,
+				to: email,
+				subject: "취돌 인증메일",
+				text: `인증번호 : ${payload}`,
+				html: `         <div style="text-align: center;">
+				<h3 style="color: #FA5882">취돌 인증 메일</h3>
+				<br />
+				<p>인증번호 :  ${payload}</p>
+			  </div>`,
+			})
+			.then((result: any) => console.log(result))
+			.catch((err: any) => console.log(err));
 	}
 	return res.status(200).json({
 		ok: true,
