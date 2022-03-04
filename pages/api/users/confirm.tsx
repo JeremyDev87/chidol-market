c4b8fb1afe0dfc0e6eb46ae925d9d41c1e7c2164
Jeremy Winchester Park <soundbrokaz@kakao.com>
2022-03-04 17:21:09 +0900

    iron-session used cookie create

--- pages/api/users/confirm.tsx
+++ pages/api/users/confirm.tsx
@@ -1,14 +1,29 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import withHandler, { ResponseType } from "@libs/server/withHandler";
 import client from "@libs/server/client";
+import { withIronSessionApiRoute } from "iron-session/next";
 
 async function handler(
 	req: NextApiRequest,
 	res: NextApiResponse<ResponseType>
 ) {
 	const { token } = req.body;
-	console.log(token);
+	const exists = await client.token.findUnique({
+		where: {
+			payload: token,
+		},
+		include: { user: true },
+	});
+	if (!exists) res.status(404).end();
+	console.log(exists);
+	req.session.user = {
+		id: exists?.userId,
+	};
+	await req.session.save();
 	res.status(200).end();
 }
 
-export default withHandler("POST", handler);
+export default withIronSessionApiRoute(withHandler("POST", handler), {
+	cookieName: "carrotSession",
+	password: process.env.IRONSESSION_PW!,
+});
