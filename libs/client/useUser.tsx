3ae3da548d6439bb2727ee03148ea7b26eb6f62b
Jeremy Winchester Park <soundbrokaz@kakao.com>
2022-03-10 13:22:07 +0900

    feat : User validation user SWR

--- libs/client/useUser.tsx
+++ libs/client/useUser.tsx
@@ -1,19 +1,12 @@
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import useSWR from "swr";
+
+const fetcher = (url: string) => fetch(url).then((response) => response.json());
 
 export default function useUser() {
-	const [user, setUser] = useState();
-	const router = useRouter();
-	useEffect(() => {
-		fetch("/api/users/me")
-			.then((response) => response.json())
-			.then((data) => {
-				// console.log(data);
-				if (!data.ok) {
-					return router.replace("/enter");
-				}
-				setUser(data.profile);
-			});
-	}, [router]);
-	return user;
+	const { data, error } = useSWR("/api/users/me", fetcher);
+
+	//router.replace("/enter");
+	return data;
 }
