8010936fb218474eb249298276deccd074c1a3a6
Jeremy Winchester Park <soundbrokaz@kakao.com>
2022-03-10 15:48:55 +0900

    <refeactor>: user validation refactoring by SWRConfig - SWRConfig를 활용해서 fetcher를 다이나믹하게 구현함

--- libs/client/useUser.tsx
+++ libs/client/useUser.tsx
@@ -2,11 +2,13 @@ import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import useSWR from "swr";
 
-const fetcher = (url: string) => fetch(url).then((response) => response.json());
-
 export default function useUser() {
-	const { data, error } = useSWR("/api/users/me", fetcher);
-	console.log(data);
-	//router.replace("/enter");
-	return data;
+	const { data, error } = useSWR("/api/users/me");
+	const router = useRouter();
+	useEffect(() => {
+		if (data && !data.ok) {
+			router.replace("/enter");
+		}
+	}, [data, router]);
+	return { user: data?.profile, isLoading: !data && !error };
 }
