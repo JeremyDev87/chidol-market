05db5e6f5160a9f5b0e408b355ea658dd2b9f811
Jeremy Winchester Park <soundbrokaz@kakao.com>
2022-03-17 13:06:29 +0900

    <feat>: Create Profile Edit module - 프로필 정보 업데이트 기능 개발

--- pages/profile/edit.tsx
+++ pages/profile/edit.tsx
@@ -2,11 +2,61 @@ import type { NextPage } from "next";
 import Button from "@components/button";
 import Input from "@components/input";
 import Layout from "@components/layout";
+import useUser from "@libs/client/useUser";
+import { useForm } from "react-hook-form";
+import { useEffect } from "react";
+import useMutation from "@libs/client/useMutation";
 
+interface EditProfileForm {
+	name?: string;
+	email?: string;
+	phone?: string;
+	formErrors?: string;
+}
+interface EditProfileResponse {
+	ok: boolean;
+	error?: string;
+}
 const EditProfile: NextPage = () => {
+	const { user } = useUser();
+
+	const {
+		register,
+		setValue,
+		handleSubmit,
+		setError,
+		formState: { errors },
+	} = useForm<EditProfileForm>();
+	const [editProfile, { data, loading }] =
+		useMutation<EditProfileResponse>("/api/users/me");
+
+	const onValid = ({ email, phone, name }: EditProfileForm) => {
+		if (email === "" && phone === "" && name === "") {
+			return setError("formErrors", {
+				message: "Email이나 Phone 둘중 하나는 채우세요",
+			});
+		}
+		editProfile({ email, phone, name });
+	};
+
+	useEffect(() => {
+		user?.email ? setValue("email", user.email) : null;
+		user?.phone ? setValue("phone", user.phone) : null;
+		user?.name ? setValue("name", user.name) : null;
+	}, [user, setValue]);
+
+	useEffect(() => {
+		if (data && !data.ok && data?.error) {
+			setError("formErrors", { message: data?.error });
+		}
+	}, [data, setError]);
+
 	return (
 		<Layout canGoBack title="Edit Profile">
-			<form className="py-10 px-4 space-y-4">
+			<form
+				className="py-10 px-4 space-y-4"
+				onSubmit={handleSubmit(onValid)}
+			>
 				<div className="flex items-center space-x-3">
 					<div className="w-14 h-14 rounded-full bg-slate-500" />
 					<label
@@ -23,19 +73,33 @@ const EditProfile: NextPage = () => {
 					</label>
 				</div>
 				<Input
-					required
+					required={false}
+					label="Name"
+					name="name"
+					type="text"
+					register={register("name")}
+				/>
+				<Input
+					required={false}
 					label="Email address"
 					name="email"
 					type="email"
+					register={register("email")}
 				/>
 				<Input
-					required
+					required={false}
 					label="Phone number"
 					name="phone"
 					type="number"
 					kind="phone"
+					register={register("phone")}
 				/>
-				<Button text="Update profile" />
+				{errors?.formErrors ? (
+					<span className="my-2 text-red-500 font-medium text-center block">
+						{errors?.formErrors?.message}
+					</span>
+				) : null}
+				<Button text={loading ? "Loading..." : "Update profile"} />
 			</form>
 		</Layout>
 	);
