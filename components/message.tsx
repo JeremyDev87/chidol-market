fca2e01fd0c47b14373e9a339d866549c5838af3
Jeremy Winchester Park <soundbrokaz@kakao.com>
2022-03-17 16:57:32 +0900

    <style>: Change message style - 내가 발송한 메세지 색 추가 - 내가 발송한 메세지 우측 정렬

--- components/message.tsx
+++ components/message.tsx
@@ -15,11 +15,16 @@ export default function Message({
 		<div
 			className={cls(
 				"flex  items-start space-x-2",
-				reversed ? "flex-row-reverse space-x-reverse" : ""
+				reversed ? "flex-row-reverse space-x-reverse text-right" : ""
 			)}
 		>
 			<div className="w-8 h-8 rounded-full bg-slate-400" />
-			<div className="w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">
+			<div
+				className={cls(
+					"w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md",
+					reversed ? "bg-yellow-400" : ""
+				)}
+			>
 				<p>{message}</p>
 			</div>
 		</div>
