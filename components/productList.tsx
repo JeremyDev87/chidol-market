b2c0c9d7b899d56fe4f705e2121453e9e2422686
Jeremy Winchester Park <soundbrokaz@kakao.com>
2022-03-17 11:05:28 +0900

    <feat>: Create ProductList Component - 제품 리스트 보여주는 화면이 많아 컴포넌트화 시킴.

---
+++ components/productList.tsx
@@ -0,0 +1,33 @@
+import { ProductWithFavCount } from "pages";
+import useSWR from "swr";
+import Item from "./item";
+
+interface ProductListProps {
+	kind: "favs" | "sales" | "purchases";
+}
+interface Record {
+	id: number;
+	product: ProductWithFavCount;
+}
+interface ProductListResponse {
+	[key: string]: Record[];
+}
+
+export default function ProductList({ kind }: ProductListProps) {
+	const { data } = useSWR<ProductListResponse>(`/api/users/me/${kind}`);
+	return (
+		<>
+			{data
+				? data[kind]?.map((record) => (
+						<Item
+							id={record.product.id}
+							key={record.id}
+							title={record.product.name}
+							price={record.product.price}
+							hearts={record.product._count.favs}
+						/>
+				  ))
+				: null}
+		</>
+	);
+}
