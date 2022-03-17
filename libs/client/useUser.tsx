22cd0370c243df30a19db60fe5692a7ac74a57d8
Jeremy Winchester Park <soundbrokaz@kakao.com>
2022-03-17 11:31:20 +0900

    <refactor>: Add Type interface on useUser - user 끌어 쓸 때 자동완성좀 써먹을려고 추가함

--- libs/client/useUser.tsx
+++ libs/client/useUser.tsx
@@ -1,14 +1,21 @@
+import { User } from "@prisma/client";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import useSWR from "swr";
 
+interface ProfileResponse {
+	ok: boolean;
+	profile: User;
+	isLoading: boolean;
+}
+
 export default function useUser() {
-	const { data, error } = useSWR("/api/users/me");
+	const { data, error } = useSWR<ProfileResponse>("/api/users/me");
 	const router = useRouter();
 	useEffect(() => {
 		if (data && !data.ok) {
 			router.replace("/enter");
 		}
 	}, [data, router]);
-	return { user: data?.profile, isLoading: !data && !error };
+	return { ok: true, user: data?.profile, isLoading: !data && !error };
 }
