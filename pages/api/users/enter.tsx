6d4b31f5dc87e2a2f5430266cdd5e1abc91227d5
Jeremy Winchester Park <soundbrokaz@kakao.com>
2022-04-05 14:40:41 +0900

    <fix>: Delete Twilio and Sendgrid Module

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,20 +1,7 @@
 import client from "@libs/server/client";
-import twilio from "twilio";
-import mail from "@sendgrid/mail";
 import withHandler, { ResponseType } from "@libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
 import { withApiSession } from "@libs/server/withSession";
-
-const nodemailer = require("nodemailer");
-const transporter = nodemailer.createTransport({
-	service: "gmail",
-	auth: {
-		user: process.env.GMAIL_ID,
-		pass: process.env.GMAIL_PW,
-	},
-});
-// mail.setApiKey(process.env.SENDGRID_KEY!);
-const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
 
 async function handler(
 	req: NextApiRequest,
@@ -46,28 +33,6 @@ async function handler(
 			},
 		},
 	});
-	// if (phone) {
-	// 	await twilioClient.messages.create({
-	// 		messagingServiceSid: process.env.TWILIO_MSID,
-	// 		to: process.env.MYPHONE!,
-	// 		body: `인증번호 : ${payload}`,
-	// 	});
-	// } else if (email) {
-	// 	const sendEmail = await transporter
-	// 		.sendMail({
-	// 			from: `soundbrokaz <soundbrokaz@gmail.com>`,
-	// 			to: email,
-	// 			subject: "취돌 인증메일",
-	// 			text: `인증번호 : ${payload}`,
-	// 			html: `         <div style="text-align: center;">
-	// 			<h3 style="color: #FA5882">Hobbyist 인증 메일</h3>
-	// 			<br />
-	// 			<p>인증번호 :  ${payload}</p>
-	// 		  </div>`,
-	// 		})
-	// 		.then((result: any) => console.log(result))
-	// 		.catch((err: any) => console.log(err));
-	// }
 	return res.status(200).json({
 		ok: true,
 	});
